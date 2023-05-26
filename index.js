e0b609c0ffba5d89d7e467ec85e592b6afb10115
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2023-05-26 18:18:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ fetch('https://642682b4d24d7e0de471791d.mockapi.io/mir/users', {
   },
   body: JSON.stringify(user)
 })
-.then(res=>res.json())
-.then(console.log(res));
+fetch('https://642682b4d24d7e0de471791d.mockapi.io/mir/users')
+      .then(response => response.json())
+      .then(json => console.log(json));
