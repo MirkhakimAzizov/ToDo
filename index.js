b16820b6fcad37c0cdf832ef2f19923495ec05d4
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2023-05-26 16:54:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 "use strict";
-fetch('https://642682b4d24d7e0de471791d.mockapi.io/mir/users')
+fetch('https://642682b4d24d7e0de471791d.mockapi.io/mir/users', {
+      method: "POST",
+      body: ["name":"MR","password":"27","id":"18"],
+})
       .then(response => response.json())
       .then(json => console.log(json))
