f73fb9796cd151017b6e9e643a41c242fedeafc8
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2023-05-26 18:48:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ let elBtn = document.querySelector(".post-btn");
 let elName = document.querySelector("name-input");
 let elPhone = document.querySelector("phone-input");
 let elProduct = document.querySelector("product-input");
+let elForm = document.querySelector("el-form");
 
-elBtn.addEventListener("click", (e)=>{
-e.preventDefault();
+elForm.addEventListener("submit",e=>{
+  e.preventDefault();
+  console.log("submit");
   let value1 = elName.value;
   console.log(value1);
 });
+
+// elBtn.addEventListener("click", (e)=>{
+// e.preventDefault();
+//   let value1 = elName.value;
+//   console.log(value1);
+// });
 
 fetch('https://642682b4d24d7e0de471791d.mockapi.io/mir/users', {
   method: 'POST',
