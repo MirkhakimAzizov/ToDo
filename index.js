9dc6b93aa14c2d2c83b70ca76a84dd5f9e43d1e7
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2023-05-26 18:14:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ let user = {
   id: 18,
 };
 
-let response = await fetch('https://642682b4d24d7e0de471791d.mockapi.io/mir/users', {
+fetch('https://642682b4d24d7e0de471791d.mockapi.io/mir/users', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json;charset=utf-8'
   },
   body: JSON.stringify(user)
-});
-
-let result = await response.json();
-console.log(result);
+})
+.then(res=>res.json())
+.then(console.log(res));
