7b155613b6498f931decffa8aa4e97cdf45d2d87
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2023-05-27 14:00:19 +0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,25 +1,6 @@
-let arr;
-async function dataBase() {
-    const response = await fetch('https://642682b4d24d7e0de471791d.mockapi.io/mir/users');
-    const database = await response.json();
-    arr = database;
+async function Data(){
+    let res = await fetch('https://642682b4d24d7e0de471791d.mockapi.io/mir/users');
+    let data = await res.json();
+    console.log(data);
 }
-
-dataBase(data, PAGE_API, index, sortName);
-searchFun();
-
-function renderUi(arr) {
-    if (arr && arr.length > 0) {
-        arr.forEach(item => {
-                    let elLi = document.createElement('li');
-                    elLi.setAttribute('class', 'card');
-                    elLi.innerHTML = `
-      
-      
-        `
-            fragment.append(elLi);
-        });
-        elList.innerHTML = '';
-        elList.append(fragment);
-    }
-}
+Data();
