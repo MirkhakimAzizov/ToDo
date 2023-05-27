fb33147982a4af8f567ba0328967a2d0d6693401
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2023-05-27 15:58:05 +0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -16,7 +16,7 @@ async function renderUi(){
             <li class="list-group-item">Phone: ${item.phone}</li>
             <li class="list-group-item">Product: ${item.product}</li>
             <span class="position-absolute" style="top: 5px; right: 5px;">ID: ${item.id}</span>
-            <button class="btn btn-primary">Bajarilmadi</button>
+            <button class=`btn ${item.st ? "btn-primary" : "btn-danger"}`>${item.st ? "Bajarildi" : "Bajarilmadi"}</button>
         `
         elList.append(elLi);
     });
