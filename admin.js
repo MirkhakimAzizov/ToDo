68bf526b147cf4a5bf18de53111a446790c121cb
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2023-05-27 14:11:58 +0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-let elList = document.querySelector('.list');
+// let elList = document.querySelector('.list');
 async function Data(){
     let res = await fetch('https://642682b4d24d7e0de471791d.mockapi.io/mir/users');
     let data = await res.json();
