96479e6d660843c28e5a30a8cf9557e7cfb50c00
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2023-05-27 15:01:47 +0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -10,7 +10,7 @@ async function renderUi(){
     let arr = await Data();
     arr.forEach(item=>{
         let elLi = document.createElement('li');
-        elLi.setAttribute('class', 'card my-3 py-2 px-4 bg-success');
+        elLi.setAttribute('class', 'card my-3 py-2 px-4 bg-secondary');
         elLi.innerHTML = `
             <li class="list-group-item">Name: ${item.name}</li>
             <li class="list-group-item">Phone: ${item.phone}</li>
